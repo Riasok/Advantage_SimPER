9464314fa8a9570aa3ce3efd1c80326888f07305
minjae <kosair@snu.ac.kr>
2025-03-24 19:03:50 +0900

    update

--- scripts/launch_sample.sh
+++ scripts/launch_sample.sh
@@ -5,6 +5,7 @@
 export CUDA_VISIBLE_DEVICES=3
 export HYDRA_FULL_ERROR=1
 export MODEL_NAME=llama3-dpo-test-D-1e-5
+export MODEL_NAME="meta-llama/Llama-3.1-8B-Instruct"
 export CKPT=/outputs/${MODEL_NAME}/FINAL
 #export CKPT=~/.cache/huggingface/hub/meta-llama/Llama-3.1-8B-Instruct
 
@@ -12,9 +13,9 @@ export HF_DATASETS_OFFLINE=1
 export HF_HUB_OFFLINE=1
 
 
-python -m train.sample $CKPT \
+python -m train.sample $MODEL_NAME \
   --gpu_count 1 \
-  --output_file outputs/${MODEL_NAME}.json \
+  --output_file outputs/bob.json \
   --datasets math500 \
   --num_samples_per_prompt 8 \
   --split test
